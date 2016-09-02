4d5cf9db7de13f80573ef94b3ea01a11288bdd6c
pablo-ziegler <pabloz18ezeiza@gmail.com>
2016-09-01 23:48:26 -0300

    codigo inicial

---
+++ platforms/android/assets/www/js/Services/services.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* Services */
+
+/*
+ http://docs.angularjs.org/api/ngResource.$resource
+ Default ngResources are defined as
+ 'get':    {method:'GET'},
+ 'save':   {method:'POST'},
+ 'query':  {method:'GET', isArray:true},
+ 'remove': {method:'DELETE'},
+ 'delete': {method:'DELETE'}
+ */
+
+var services = angular.module('ngdemo.services', ['ngResource']);
+
+/* USUARIO
+Lo dejo para consultar.
+services.factory('UsuarioFactory', function ($resource) {
+	return $resource('/tracker/rest/usuario/:id', {}, {
+        show: { method: 'GET' },
+        remove: { method: 'DELETE' },
+        update: { method: 'PUT' },
+        create: { method: 'POST' },
+        query: { method: 'GET', params: {}, isArray: true },
+    });
+});
+*/
---
+++ platforms/ios/www/js/app.js
@@ -0,0 +1,73 @@
+// Ionic ema App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'ema' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'ema.controllers.controllers' is found in controllers/controllers.js
+angular.module('ema', ['ionic', 'ema.controllers.controllers','ema.services.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'partials/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'partials/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'partials/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'partials/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'partials/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
