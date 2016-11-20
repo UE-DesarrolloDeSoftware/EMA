ccbefa8ac24e6da836b5a042fdd233a32f7a99cf
pablo-ziegler <pabloz18ezeiza@gmail.com>
2016-11-20 14:01:48 -0300

    comentado en preguntas frecuentes error

--- www/js/controllers/mapAdminController.js
+++ www/js/controllers/mapAdminController.js
@@ -1,7 +1,7 @@
 
 angular.module('ema.controllers')
 
-.controller('MapAdminController',function($scope, $cordovaGeolocation, $ionicPopup,ZonaEstacionamientoServices, VendedorService ){
+.controller('MapAdminController',function($scope, $cordovaGeolocation, $ionicPopup,$q,ZonaEstacionamientoServices, VendedorService ){
 
     var coordinates = [],
      markersLocation= [],
@@ -21,6 +21,7 @@ angular.module('ema.controllers')
         
         var zonesparking = viewZoneParking(map);
         var puntosventa = viewPointsSales(map);
+        
         //var geocodeService = L.esri.Geocoding.geocodeService();
        
 
@@ -124,11 +125,50 @@ angular.module('ema.controllers')
           if (type === 'marker') {
 
               var lat = layer.getLatLng().lat,
-              lng = layer.getLatLng().lng;
-              
+              lng = layer.getLatLng().lng,
+              address =  'http://nominatim.openstreetmap.org/reverse?format=json&lat='+lat+'&lon='+lng+'&zoom=18&addressdetails=1';
+              var addressfromcoordinates = JSON.stringify(address);
+              var coords = e.layer._latlng;
+              var tempMarker = drawnItems.addLayer(e.layer);
 
+              var contenido = "<b>You are here</b><br> LatLong: "+  lat.toString()+lng.toString()+' <br> <a href=http://nominatim.openstreetmap.org/reverse?format=json&lat='+lat.toString()+'&lon='+lng.toString()+'&addressdetails=1">Address</a>';
+
+
+              var popupContent = '<form role="form" id="form" enctype="multipart/form-data" class = "form-horizontal" onsubmit="addMarker()">'+
+              '<div class="form-group">'+
+                  '<label class="control-label col-sm-5"><strong>Date: </strong></label>'+
+                  '<input type="date" placeholder="Required" id="date" name="date" class="form-control"/>'+ 
+              '</div>'+
+              '<div class="form-group">'+
+                  '<label class="control-label col-sm-5"><strong>Gender: </strong></label>'+
+                  '<select class="form-control" id="gender" name="gender">'+
+                    '<option value="Male">Male</option>'+
+                    '<option value="Female">Female</option>'+
+                    '<option value="Other">Other</option>'+
+                  '</select>'+ 
+              '</div>'+
+              '<div class="form-group">'+
+                  '<label class="control-label col-sm-5"><strong>Age: </strong></label>'+
+                  '<input type="number" min="0" class="form-control" id="age" name="age">'+ 
+              '</div>'+
+              //...
+              '<div class="form-group">'+
+                  '<label class="control-label col-sm-5"><strong>Description: </strong></label>'+
+                  '<textarea class="form-control" rows="6" id="descrip" name="descript">...</textarea>'+
+              '</div>'+
+              '<input style="display: true;" type="text" id="lat" name="lat" value="'+lat+'" />'+
+              '<input style="display: true;" type="text" id="lng" name="lng" value="'+lng+'" />'+
+              '<div class="form-group">'+
+                '<div style="text-align:center;" class="col-xs-4 col-xs-offset-2"><button type="button" class="btn">Cancel</button></div>'+
+                '<div style="text-align:center;" class="col-xs-4"><button type="submit" value="submit" class="btn btn-primary trigger-submit">Submit</button></div>'+
+              '</div>'+
+              '</form>';
+              tempMarker.bindPopup(contenido,{
+                keepInView: true,
+                closeButton: true
+                }).openPopup();
             
-            
+            console.log(addressfromcoordinates);
             // here you add it to a layer to display it in the map
               drawnItems.addLayer(layer);
               editableLayers.addLayer(layer);  
@@ -136,8 +176,8 @@ angular.module('ema.controllers')
               var markerObject = {};
               markerObject.location = [lat,lng];
               var myPopup = L.popup()
-              var popupContent = '<ion-modal-view style="width: 40%; height: 60%;top: 5%;">    <ion-header-bar class="bar-calm">        <h6 class="title">Nuevo Punto de Venta</h6>                </div>    </ion-header-bar>    <ion-content class="padding">        <form name="puntoVentaForm" ng-submit="addVendedor()">            <div class="list">                    <label class="item item-input">                    <input type="number" placeholder="CUIT" ng-model="markerObject.cuit" >                </label>                <label class="item item-input">                    <input type="number" placeholder="ID USUARIO" ng-model="markerObject.usuario_id" >                </label><label> <input type="number" placeholder="ID USUARIO"  disabled> </label>                <label class="item item-input">                    <input type="text" placeholder="business Name" ng-model="markerObject.business_name" >                </label>                                                </div>            <div class="padding">                <button class="button button-block button-balanced" ng-disabled="puntoVentaForm.$invalid">Save Punto Venta</button>            </div>        </form>   </ion-content></ion-modal-view>';
-              myPopup.setContent(popupContent);
+              //var popupContent = '<ion-modal-view style="width: 40%; height: 60%;top: 5%;">    <ion-header-bar class="bar-calm">        <h6 class="title">Nuevo Punto de Venta</h6>                </div>    </ion-header-bar>    <ion-content class="padding">        <form name="puntoVentaForm" ng-submit="addVendedor()">            <div class="list">                    <label class="item item-input">                    <input type="number" placeholder="CUIT" ng-model="markerObject.cuit" >                </label>                <label class="item item-input">                    <input type="number" placeholder="ID USUARIO" ng-model="markerObject.usuario_id" >                </label><label> <input type="number" placeholder="ID USUARIO"  disabled> </label>                <label class="item item-input">                    <input type="text" placeholder="business Name" ng-model="markerObject.business_name" >                </label>                                                </div>            <div class="padding">                <button class="button button-block button-balanced" ng-disabled="puntoVentaForm.$invalid">Save Punto Venta</button>            </div>        </form>   </ion-content></ion-modal-view>';
+              myPopup.setContent(contenido);
               layer.bindPopup(myPopup).openPopup();
 
               VendedorService.addVendedor(markerObject).then(function () {})
@@ -202,9 +242,48 @@ angular.module('ema.controllers')
               })
           });
 
-
+    function addr_search() {
+      var inp = document.getElementById("addr");
+    
+      $.getJSON('http://nominatim.openstreetmap.org/search?format=json&lat='+lat+'&lon='+lng+'&zoom=18&q=' + inp.value, function(data) {
+      })
+    };
 
     // SITUAR EN POSICION ACTUAL
+    $scope.locate = function () {
+
+        // LLAMADA ASINCRONICA
+        var posicionActual = getActualLocation();
+
+        posicionActual.then(function (actualPosition) {
+
+            $scope.map.setView(actualPosition, 18);
+
+            addMarkerAuto($scope.map, actualPosition);
+
+        }, function (reason) {
+            alert('Failed: ' + reason);
+        });
+    };
+
+
+    function getActualLocation () {
+
+        return $q(function (resolve, reject) {
+            $cordovaGeolocation
+            .getCurrentPosition()
+            .then(function (position) {
+
+                resolve(new L.LatLng(position.coords.latitude, position.coords.longitude));
+
+            }, function (err) {
+                // error
+                reject(err.message);
+            });
+        });
+    }
+
+    /*
     $scope.locate = function(){
 
         $cordovaGeolocation
@@ -232,7 +311,8 @@ angular.module('ema.controllers')
               });
 
        };
+       */
     })
-
+    
 
 
